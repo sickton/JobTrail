30b3ffa32168bca9334870e99ac2456bc4f1e175
Srivathsa Mantrala <svmantra@ncsu.edu>
2026-03-19 15:55:08 -0400

    feat: Created repository layers for all the entity database communication #9

--- jobtrail-api/src/main/java/com/jobtrail/repository/UserRepository.java
+++ jobtrail-api/src/main/java/com/jobtrail/repository/UserRepository.java
@@ -1,4 +1,28 @@
 package com.jobtrail.repository;
 
-public class UserRepository {
+import com.jobtrail.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * @author Srivathsa Mantrala
+ * Repository interface to handle the DB communication
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Method that finds a user in database using their username
+     * @param username username of the user
+     * @return mapped user entity
+     */
+    Optional<User> findByUsername(String username);
+
+    /**
+     * Method that checks if the username exists in the database
+     * @param username username of the user
+     * @return true if username exists, else false
+     */
+    boolean existsByUsername(String username);
 }
